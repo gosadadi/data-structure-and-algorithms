89fb7c5c819066b8d02a1f7d1b61c93cc463f614
gosa_dadi <gosa.d.dadi@gmail.com>
2023-02-05 07:11:44 -0800

    modified

--- dataStructure/hashTables.js
+++ dataStructure/hashTables.js
@@ -1,9 +1,24 @@
-function hash(key, arrayLen) {
-    let total = 0;
-    for (let char of key) {
-        let value = char.charCodeAt(0) - 96;
-        total = (total + value) % arrayLen;
+class HashTable {
+    constructor(size = 17) {
+        this.keyMap = new Array(size);
     }
-    return total;
+    _hash(key){
+        let total = 0;
+        let wierd_prime = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++){
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96
+            total = (total + wierd_prime + value) % this.keyMap.length;
+        }
+        return total;
+    }
+    set(key,value) {
+        let index = this._hash(key);
+        return index;
+    }
+    get(key) {
+
+    }
 }
-console.log(hash("rosa",4));
+let ht = new HashTable();
+ht.set("hello world", "goodbye!!");
