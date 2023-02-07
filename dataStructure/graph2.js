fcb15ab5bc9333e02811a42e66be61e4923c42a9
gosa_dadi <gosa.d.dadi@gmail.com>
2023-02-06 19:26:02 -0800

    first commit

--- dataStructure/graph2.js
+++ dataStructure/graph2.js
@@ -6,6 +6,11 @@ class Graph {
     addVertex(vertex) {
         if(!this.adjacencyList[vertex])this.adjacencyList[vertex] = [];
     }
+    addEdge(v1,v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[2].push(v1);
+    }
 }
 let newGraph = new Graph();
 newGraph.addVertex("Tokyo");
+console.log(newGraph);
