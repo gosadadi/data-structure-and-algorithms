a3634a4be2d2dcaada6a0bf3fcadb94187e02ff1
gosa_dadi <gosa.d.dadi@gmail.com>
2023-02-07 19:12:18 -0800

    updated

--- dataStructure/graph2.js
+++ dataStructure/graph2.js
@@ -8,9 +8,21 @@ class Graph {
     }
     addEdge(v1,v2) {
         this.adjacencyList[v1].push(v2);
-        this.adjacencyList[2].push(v1);
+        this.adjacencyList[v2].push(v1);
+    }
+    remvoeEdge(v1,v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+            v => v !==vertex2
+        );
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+            v => v !==vertex1
+        );
     }
 }
 let newGraph = new Graph();
 newGraph.addVertex("Tokyo");
+newGraph.addVertex("DALLAS");
+newGraph.addVertex("Aspen");
+newGraph.addEdge("Dallas", "Tokyo")
+newGraph.addEdge("Dallas", "Aspen")
 console.log(newGraph);
