4fb97c6f8ac68a43c2ae8cbbad162750ffa91f6b
gosa_dadi <gosa.d.dadi@gmail.com>
2023-02-09 14:00:48 -0800

    first commit

--- dataStructure/graph2.js
+++ dataStructure/graph2.js
@@ -4,25 +4,46 @@ class Graph {
         this.adjacencyList = {};
     }
     addVertex(vertex) {
-        if(!this.adjacencyList[vertex])this.adjacencyList[vertex] = [];
+        if(!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
     }
     addEdge(v1,v2) {
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
-    remvoeEdge(v1,v2) {
+    removeEdge(v1, v2) {
         this.adjacencyList[v1] = this.adjacencyList[v1].filter(
-            v => v !==vertex2
+            v => v !==v2
         );
         this.adjacencyList[v2] = this.adjacencyList[v2].filter(
-            v => v !==vertex1
+            v => v !==v1
         );
+    }
+    removeVertex(vertex) {
+        while(this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
+    traverse(vertex) {
+        
     }
 }
 let newGraph = new Graph();
 newGraph.addVertex("Tokyo");
-newGraph.addVertex("DALLAS");
+newGraph.addVertex("Dallas");
 newGraph.addVertex("Aspen");
-newGraph.addEdge("Dallas", "Tokyo")
-newGraph.addEdge("Dallas", "Aspen")
+newGraph.addVertex("Los Angeles");
+newGraph.addVertex("Hong Kong");
+newGraph.addEdge("Dallas", "Tokyo");
+newGraph.addEdge("Dallas", "Aspen");
+newGraph.addEdge("Hong Kong", "Tokyo");
+newGraph.addEdge("Hong Kong", "Dallas");
+newGraph.addEdge("Los Angeles", "Hong Kong");
+newGraph.addEdge("Los Angeles", "Aspen");
+newGraph.removeEdge("Dallas", "Aspen");
+newGraph.removeEdge("Dallas", "Tokyo");
+newGraph.removeVertex("Hong Kong");
 console.log(newGraph);
